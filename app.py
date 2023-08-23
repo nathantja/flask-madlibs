ba22fd68a228de770f959131b2b3c17029095f5a
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-23 18:09:59 -0500

    homepage created

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
 
+STORY = silly_story
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def homepage():
+    return render_template("questions.html", prompts= STORY.prompts)
